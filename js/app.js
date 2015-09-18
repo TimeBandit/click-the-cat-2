2d406bc3207eb90904079ec0a8a6cc22f1bf0afc
imran_nazir <imran.nazir@yahoo.co.uk>
2015-09-18 15:48:31 +0100

    added foreach bindings & with contexts

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
 // knockout synchronizes the view and the model so that you dont have to
-var ViewModel = function() {
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
+    this.nickNames = ko.observableArray([
+        'ammar',
+        'akbar',
+        'antony',
+        'akira',
+        'anan'
+    ]);
 
-    this.incrementCounter = function () {
-    	this.clickCount(this.clickCount() + 1);
+    this.level = ko.computed(function() {
+        if (this.clickCount() <= 10) {
+            return 'Newborn';
+        } else {
+            return 'Infant';
+        };
+    }, this);
+}
+
+var ViewModel = function() {
+	self = this;
+	self.currentCat = ko.observable(new Cat);
+
+    self.incrementCounter = function() {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 };
 
